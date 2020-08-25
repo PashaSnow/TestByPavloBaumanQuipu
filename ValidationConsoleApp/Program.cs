c865f54ee04f4780f974a01c8b7ba3834c7f52c3
Pavlo Bauman <baumanfromkpi@gmail.com>
2020-08-25 23:07:13 +0300

    Quipu - ASP.NET task

---
+++ ValidationConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Validation
+{
+    #region My task for console application
+    //  Необходимо реализовать сервис, который должен осуществлять валидацию имени и пароля пользователя(консольное приложение). 
+    //  Следует предусмотреть логику, чтобы сервис возвращал положительный ответ, только, если имя пользователя представлено в виде адреса и пароль не содержит специальных символов.
+
+    // виходячи з контексту задачі я не використовував рішнень майкрософт на подобі "IValidatableObject" для повноціних програм, метод "IsValid" викликаю в проекті WebValidation для організації валідації, хоча майкрософт рекомендує використовувати "ASP.NET Identity"
+    #endregion
+    public class Program
+    {
+        public static bool IsValid(string login, string password)
+        {
+            string patternLog = @"^([\w\._]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,3})$";
+            string patternPassword = @"^[A-Za-z0-9]{3,60}$";
+
+            return Regex.IsMatch(login, patternLog) && Regex.IsMatch(password, patternPassword);
+        }
+        static void Main()
+        {
+            
+            Console.WriteLine("Login");
+            string inputLog = Console.ReadLine();
+
+            Console.WriteLine("Password");
+            string inputPassword = Console.ReadLine();
+
+            // result
+            Console.WriteLine(IsValid(inputLog, inputPassword));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WebValidation/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Validation;
+using WebValidation.Models;
+
+namespace WebValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// Get data from user
+        /// </summary>
+        /// <param name="logInInfo"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Index(LoginModel logInInfo)
+        {
+            string message = "";
+
+            if (!(string.IsNullOrEmpty(logInInfo.Login) && string.IsNullOrEmpty(logInInfo.Password)))
+            {
+                 
+                if (Program.IsValid(logInInfo.Login, logInInfo.Password)) // Validation
+                {
+                    message = "true - successful validation";
+                }
+                else
+                    message = "false - validation failed";
+            }
+            else
+            {
+                message = "false - login or password is null or empty";
+            }
+            ViewData["Message"] = message;
+            return View("Index");
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
